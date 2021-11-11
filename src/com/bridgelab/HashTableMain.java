eda2af2adcea6e5f01791e22067395657716a18a
pitu <ashishkumarvish.432@gmail.com>
2021-11-11 15:38:44 +0530

    Method to find frequency of paragraph word

--- src/com/bridgelab/HashTableMain.java
+++ src/com/bridgelab/HashTableMain.java
@@ -4,28 +4,26 @@ import java.util.Locale;
 
 public class HashTableMain {
     public static void main(String[] args) {
-        System.out.println("Welcome to HashTable");
         MyHashTable<String, Integer> hashTable = new MyHashTable();
-        String sentence = "To be or not  to be";
+        String sentence = "Paranoids are not paranoid because they are paranoid but they keep putting themselves deliberately avoid situations";
+
         // storing given sentence in an array.
         // converting the sentence to lower case and removing spaces.
         String[] sentenceArray = sentence.toLowerCase().split(" ");
         // Iterating over the array.
-        for (String word :  sentenceArray) {
+        for (String word : sentenceArray) {
             Integer count = hashTable.get(word);
 
             if (count == null) {
 
-                count= 1;
+                count = 1;
 
 
-            }
-            else {
+            } else {
                 count = count + 1;
                 hashTable.add(word, count);
 
             }
-
         }
         System.out.println(hashTable);
     }
--- src/com/bridgelab/MyHashMap.java
+++ src/com/bridgelab/MyHashMap.java
@@ -1,23 +1,22 @@
 package com.bridgelab;
 
-public class MyHashTable<K, V> {
+public class MyHashMap<K, V> {
     MyMapNode head;
     MyMapNode tail;
+
     /**
-     * Purpose: Method to get the word from LinkList;
+     * Purpose : Method to get  the word from Linked List;
      *
-     * @param word:Word returned.
+     * @param word: word returned.
      */
     public V get(K word) {
-        MyMapNode<K, V> myNode = searchNode(word);
-        return (myNode== null) ? null : myNode.getValu();
+        MyMapNode<K, V> mapNode = searchNode(word);
+        return (myNode == null) ? null : mapNode.getValue();
 
     }
+
     /**
-     * Purpose to search word from Linked List.
-     *
-     * @param word : word to search.
-     *
+     * @param word: word to search.
      */
     public MyMapNode<K, V> searchNode(K word) {
         MyMapNode<K, V> currentNode = head;
@@ -26,56 +25,51 @@ public class MyHashTable<K, V> {
             position++;
             if (currentNode.getKey().equals(word)) {
                 return currentNode;
+
             }
             currentNode = currentNode.getNext();
-
 
         }
         return currentNode;
 
-
-
     }
+
     /**
-     * Purpose : Method to add key and valu to hash table.
-     *
-     * @param Key : word to be added.
-     * @param value: Frequency of word.
-     *
+     * Purpose : Method to add value and key  to hash table.
+     *  @param key :   word to be added.
+     * @param value : frequency.
+     * @return
      */
-    public void add(K Key, V value) {
-        MyMapNode<K, V> mapNode = searchNode(Key);
+    public String add(K key, V value) {
+        MyMapNode<K, V> myNode = searchNode(key);
         if (myNode == null) {
-            myNode = new MyMapNode<>(Key, value);
+            myNode = new MyMapNode<>(key, value);
             this.append(myNode);
 
-        }else {
-            myNode.setValu(value);
+        } else {
+            myNode.setValue(value);
 
         }
-
         /**
-         * Purpose: Method to append value to Linked list.
+         * Purpose: Method to append value to Linked List.
          *
          * @param myNode: value to append.
-         *
          */
-        private void append(MyMapNode<K, V> myNode) {
+        private void append (MyMapNode < K, V > myNode){
             if (this.head == null)
                 this.head = myNode;
-            if(this.tail == null)
+            if (this.tail == null)
                 this.tail = myNode;
             else {
                 this.tail.setNext(myNode);
                 this.tail = myNode;
-
             }
         }
         @Override
-         public String toString() {
-            return "MyLinkedListNode{" + head + '}';
+        public String toString() {
+            return "MyLinkedListNodes{" + head + '}';
 
         }
-
     }
+
 }
